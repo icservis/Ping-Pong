c11ebd4edfc845c5aca2a844e97177451094d9ee
Libor Kučera <developer@ic-servis.com>
2019-02-04 15:54:40 +0100

    TableViewController

--- ping-pong/ping-pong/GameViewController.swift
+++ ping-pong/ping-pong/GameViewController.swift
@@ -15,7 +15,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
+        if let view = view as? SKView {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
@@ -24,11 +24,10 @@ class GameViewController: UIViewController {
                 // Present the scene
                 view.presentScene(scene)
             }
-            
+
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsNodeCount = false
         }
     }
 
---
+++ ping-pong/ping-pong/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  ping-pong
+//
+//  Created by Libor Kučera on 04/02/2019.
+//  Copyright © 2019 IC Servis, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
