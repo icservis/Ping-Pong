a85d07b85096ee130e4f8e9c395844f2e195eb61
Libor Kučera <developer@ic-servis.com>
2019-02-06 21:28:39 +0100

    Score

--- ping-pong/ping-pong/GameScene.swift
+++ ping-pong/ping-pong/GameScene.swift
@@ -13,8 +13,14 @@ class GameScene: SKScene {
     var ball: SKSpriteNode!
     var player: SKSpriteNode!
     var enemy: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
 
     let playPing = SKAction.playSoundFileNamed("ball-ping.caf", waitForCompletion: false)
+    var score: (player: Int, enemy: Int) = (0, 0) {
+        didSet {
+            scoreLabel.text = "\(score.player) : \(score.enemy)"
+        }
+    }
 
     private lazy var sceneBorder: SKPhysicsBody = {
         let sceneBorder = SKPhysicsBody(edgeLoopFrom: frame)
@@ -29,14 +35,34 @@ class GameScene: SKScene {
         ball = (childNode(withName: "ball") as! SKSpriteNode)
         player = (childNode(withName: "player") as! SKSpriteNode)
         enemy = (childNode(withName: "enemy") as! SKSpriteNode)
+        scoreLabel = (childNode(withName: "score") as! SKLabelNode)
 
         ball.physicsBody?.applyImpulse(CGVector(dx: 75, dy: 75))
         physicsBody = sceneBorder
 
         physicsWorld.contactDelegate = self
         let _ = playPing
+
+        startGame()
     }
-    
+
+    func startGame() {
+        score = (player: 0, enemy: 0)
+    }
+
+    func resetBall() {
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        ball.position = CGPoint(x: 0, y: 0)
+    }
+
+    func addScore(playerWhoWon: SKSpriteNode) {
+        if playerWhoWon == enemy {
+            score.enemy += 1
+        }
+        if playerWhoWon == player {
+            score.player += 1
+        }
+    }
     
     func touchDown(atPoint position : CGPoint) {
         let moveAction = SKAction.moveTo(x: position.x, duration: 0.1)
@@ -72,6 +98,18 @@ class GameScene: SKScene {
         // Called before each frame is rendered
         let followBall = SKAction.moveTo(x: ball.position.x, duration: 0.5)
         enemy.run(followBall)
+
+        let offset: CGFloat = 70
+        if ball.position.y <= player.position.y - offset {
+            addScore(playerWhoWon: enemy)
+            resetBall()
+            ball.physicsBody?.applyImpulse(CGVector(dx: -75, dy: -75))
+        }
+        if ball.position.y >= enemy.position.y + offset {
+            addScore(playerWhoWon: player)
+            resetBall()
+            ball.physicsBody?.applyImpulse(CGVector(dx: 75, dy: 75))
+        }
     }
 }
 
